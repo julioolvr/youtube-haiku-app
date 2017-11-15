22935a2918aafdb41f887afc6860765ca4b77d2e
Julio Olivera <julio.olvr@gmail.com>
2017-11-14 22:10:26 -0500

    Basic video webview player

--- App.js
+++ App.js
@@ -1,40 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native';
 import Swiper from 'react-native-swiper';
 
 export default class App extends React.Component {
   state = {
-    messages: ['First', 'Second', 'Third'],
+    videos: ['T-b3-8GDR8E', 'UZDuGjvXd1Q', 'Y9nDagqKL7Q'],
     index: 0
   };
 
   render() {
     return (
-      <Swiper
-        loop={false}
-        index={this.state.index}
-        onIndexChanged={i => {
-          this.setState(state => ({
-            messages: [...state.messages, `Other ${i}`],
-            index: i
-          }));
-        }}
-      >
-        {this.state.messages.map(message => (
-          <View key={message} style={styles.container}>
-            <Text> {message} </Text>
-          </View>
+      <Swiper loop={false} index={this.state.index} showsPagination={false}>
+        {this.state.videos.map(videoId => (
+          <WebView
+            key={videoId}
+            source={{
+              uri: `https://www.youtube.com/embed/${videoId}?autoplay=1`
+            }}
+          />
         ))}
       </Swiper>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
