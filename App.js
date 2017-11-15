2fa5200bae0415a1c0ab335017f1335e735e4d48
Julio Olivera <julio.olvr@gmail.com>
2017-11-15 18:33:11 -0500

    Autoplay first video

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { WebView } from 'react-native';
 import Swiper from 'react-native-swiper';
+import createEmbedHtml from './embedded.html.js';
+
+const patchPostMessageFunction = function() {
+  var originalPostMessage = window.postMessage;
+
+  var patchedPostMessage = function(message, targetOrigin, transfer) {
+    originalPostMessage(message, targetOrigin, transfer);
+  };
+
+  patchedPostMessage.toString = function() {
+    return String(Object.hasOwnProperty).replace(
+      'hasOwnProperty',
+      'postMessage'
+    );
+  };
+
+  window.postMessage = patchedPostMessage;
+};
+
+const patchPostMessageJsCode = '(' + String(patchPostMessageFunction) + ')();';
 
 export default class App extends React.Component {
   state = {
@@ -8,15 +28,26 @@ export default class App extends React.Component {
     index: 0
   };
 
+  onWebviewMessage(message) {
+    switch (message) {
+    case 'onReady':
+      this.webview.postMessage('playVideo');
+      break;
+    }
+  }
+
   render() {
     return (
       <Swiper loop={false} index={this.state.index} showsPagination={false}>
-        {this.state.videos.map(videoId => (
+        {this.state.videos.map((videoId, i) => (
           <WebView
             key={videoId}
-            source={{
-              uri: `https://www.youtube.com/embed/${videoId}?autoplay=1`
-            }}
+            ref={webview => i === 0 && (this.webview = webview)}
+            onMessage={e => this.onWebviewMessage(e.nativeEvent.data)}
+            injectedJavaScript={patchPostMessageJsCode}
+            mediaPlaybackRequiresUserAction={false}
+            allowsInlineMediaPlayback={true}
+            source={{ html: createEmbedHtml(videoId) }}
           />
         ))}
       </Swiper>
---
+++ embedded.html.js
@@ -0,0 +1,72 @@
+// TODO: Use some real templating library
+export default function(videoId) {
+  return `
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+      <style>
+        body {
+          margin: 0;
+          padding: 0;
+          width: 100%;
+          height: 100%;
+          background-color: #000000;
+        }
+
+        html {
+          margin: 0;
+          padding: 0;
+          width: 100%;
+          height: 100%;
+          background-color: #000000;
+        }
+
+        .embed-container iframe,
+        .embed-container object,
+        .embed-container embed {
+          border: none;
+          position: absolute;
+          top: 0;
+          left: -100;
+          width: 100% !important;
+          height: 100% !important;
+        }
+      </style>
+    </head>
+
+    <body>
+        <div class="embed-container">
+      <iframe width="100%" height="100%" id="player" src="https://www.youtube.com/embed/${
+  videoId
+}?autoplay=1&controls=0&playsinline=1&showinfo=0&rel=0&enablejsapi=1&modestbranding=1"></iframe></div>
+      <script src="https://www.youtube.com/iframe_api"></script>
+
+      <script>
+        var player;
+        document.addEventListener('message', onMessageReceived);
+
+        YT.ready(function () {
+          player = new YT.Player('player', {
+            events: { onReady: onReady }
+          });
+
+        });
+
+        function onReady() {
+          postMessage('onReady');
+        }
+
+        function onMessageReceived(e) {
+          switch (e.data) {
+            case 'playVideo':
+              player.playVideo();
+              break;
+          }
+        }
+      </script>
+    </body>
+
+    </html>
+  `;
+}
